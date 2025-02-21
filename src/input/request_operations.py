924d080fbd2a771c970acbd0601f96065f915a95
Erenco19 <erenbasaran50@gmail.com>
2025-02-21 23:30:52 +0000

    edited the gitignore file, able to connect to the ideasoft api for dynamic product code retrieval from the system

--- src/input/request_operations.py
+++ src/input/request_operations.py
@@ -1,18 +1,40 @@
 import requests
-import os
-from dotenv import load_dotenv
+import json
 
-load_dotenv()
 
-client_id = os.getenv("ideasoft_client_id")
-client_secret = os.getenv("ideasoft_client_secret")
-redirect_uri = os.getenv("ideasoft_redirect_uri")
+def get_access_token():
+    response = requests.get("http://127.0.0.1:5000/get_token")
+    token_data = response.json()
+    return token_data["access_token"]
 
-# Construct the URL properly without unnecessary newline characters
-url = f"https://www.evan.com/panel/auth?client_id={client_id}&response_type=code&state=2b33fdd45jbevd6nam&redirect_uri={redirect_uri}"
 
-# Make the GET request
-response = requests.get(url)
+def get_all_products(brand_id):
+    url = "https://evan.myideasoft.com/admin-api/products"
+    headers = {"Authorization": f"Bearer {get_access_token()}"}
+    products = []
+    page = 1
 
-# Print the response
-print(response.text)
+    while True:
+        params = {"brand": brand_id, "limit": 100, "page": page}
+        response = requests.get(url, headers=headers, params=params)
+        print(f"Got page {page}")
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+            break
+
+        data = response.json()
+
+        if not data:  # if data returns empty, then that's all
+            break
+
+        products.extend(item['sku'] for item in data if 'sku' in item)
+        page += 1  # go to next page
+
+    return products
+
+
+if __name__ == "__main__":
+    brand_id = 38
+    all_skus = get_all_products(brand_id)
+
+    print(json.dumps(all_skus, indent=4, ensure_ascii=False))
