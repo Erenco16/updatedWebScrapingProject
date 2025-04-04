52dda2e9c15be819b393022e2e72532d308d798b
Erenco19 <erenbasaran50@gmail.com>
2025-04-04 01:06:16 +0100

    Notification to be sent to my personal email address once the script has started.

--- src/main.py
+++ src/main.py
@@ -217,6 +217,7 @@ def is_cookie_valid(cookie_file, expiry_time):
     )
 
 def main():
+    send_mail_with_excel("erenbasaran50@gmail.com", OUTPUT_FILE)
     global cookies
     login_thread = threading.Thread(target=refresh_login, daemon=True)
     login_thread.start()
