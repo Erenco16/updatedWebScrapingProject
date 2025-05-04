077ec5c0b5c3706f686958a637572a2472fd7b3f
Erenco19 <erenbasaran50@gmail.com>
2025-05-04 12:01:37 +0100

    renamed the selenium-hub service to avoid conflicts

--- src/main.py
+++ src/main.py
@@ -218,6 +218,7 @@ def is_cookie_valid(cookie_file, expiry_time):
 
 def main():
     global cookies
+    send_mail_with_excel("erenbasaran50@gmail.com", OUTPUT_FILE)
     login_thread = threading.Thread(target=refresh_login, daemon=True)
     login_thread.start()
 
