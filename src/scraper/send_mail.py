c759f7a105e8f2c92a7b5b78808c199b818d04b7
Erenco19 <erenbasaran50@gmail.com>
2025-05-17 11:25:24 +0100

    separated login and scraper logic

--- src/scraper/send_mail.py
+++ src/scraper/send_mail.py
@@ -6,6 +6,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def send_mail_without_excel(recipient_email,subject= "Hafele Guncel Stoklar", content= r"Guncel stoklari iceren .xlsx dosyasini ekte bulabilirsiniz."):
+
+    sender_email = os.getenv("gmail_sender_email")
+    app_password = os.getenv("gmail_app_password")
+
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg.set_content(content)
+
+    # create today's date for the product_info_table
+    today_s_date = str(date.today()).replace('-', '_')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(sender_email, app_password)
+        smtp.send_message(msg)
+        print("Done!")
 
 def send_mail_with_excel(recipient_email, excel_file):
     subject = "Hafele Guncel Stoklar"
